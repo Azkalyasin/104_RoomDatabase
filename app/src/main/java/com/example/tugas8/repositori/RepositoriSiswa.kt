ec3abb9d9be735af5749a70c0abda22ee2c7c47c
Azkalyasin <yasinadzkiaazkal@gmail.com>
2025-12-04 16:56:07 +0700

    penambahan is ke dalam offline repository siswa

--- app/src/main/java/com/example/tugas8/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/tugas8/repositori/RepositoriSiswa.kt
@@ -24,4 +24,6 @@ class OfflineRepositoriSiswa(
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
 
     //edit
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
 }
