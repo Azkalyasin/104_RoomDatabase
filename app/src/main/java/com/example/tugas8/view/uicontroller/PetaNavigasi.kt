3ef03410bdb965dd248e8d2ca7e6bb83604bb650
Azkalyasin <yasinadzkiaazkal@gmail.com>
2025-12-04 16:58:02 +0700

    menambhakan navigasi detail siswa

--- app/src/main/java/com/example/tugas8/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/tugas8/view/uicontroller/PetaNavigasi.kt
@@ -5,11 +5,15 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.tugas8.view.DetailSiswaScreen
 import com.example.tugas8.view.EntrySiswaScreen
 import com.example.tugas8.view.HomeScreen
+import com.example.tugas8.view.route.DestinasiDetailSiswa
 import com.example.tugas8.view.route.DestinasiEntry
 import com.example.tugas8.view.route.DestinasiHome
 
@@ -29,11 +33,26 @@ fun HostNavigasi(
         composable(DestinasiHome.route){
             HomeScreen(
                 navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+                navigateToItemUpdate = {
+                    navController.navigate("${DestinasiDetailSiswa.route}/${it}")
+                }
             )
         }
         composable(DestinasiEntry.route){
             EntrySiswaScreen(navigateBack = { navController.popBackStack()})
         }
+
+        composable(route = DestinasiDetailSiswa.routeWithArgs,
+            arguments = listOf(navArgument(DestinasiDetailSiswa.itemIdArg){
+                type = NavType.IntType
+            })
+        ){
+            DetailSiswaScreen(
+                navigateBack = {navController.navigateUp()},
+                navigateToEditItem = {}
+            )
+        }
+
 
     }
 }
